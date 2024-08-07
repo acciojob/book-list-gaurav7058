43ac163ed06a819f7b587828a735317624c05e6c
gaurav7058 <146345401+gaurav7058@users.noreply.github.com>
2024-08-07 12:16:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,52 +1,52 @@
-let employees = [];
-let nextId = 1;
+// let employees = [];
+// let nextId = 1;
 
-document.getElementById("submit").addEventListener("click",(event)=>{
-  event.preventDefault();
-  const title = document.getElementById("title").value.trim();
-  const author = document.getElementById("author").value.trim();
-  const isbn = document.getElementById("isbn").value.trim();
+// document.getElementById("submit").addEventListener("click",(event)=>{
+//   event.preventDefault();
+//   const title = document.getElementById("title").value.trim();
+//   const author = document.getElementById("author").value.trim();
+//   const isbn = document.getElementById("isbn").value.trim();
   
   
-  if(!title || !author || !isbn){
-    alert("Please fill in all fields");
-  }
-   else{
+//   if(!title || !author || !isbn){
+//     alert("Please fill in all fields");
+//   }
+//    else{
 
-      addEmployee(title, author, isbn);
-  }
-}) 
+//       addEmployee(title, author, isbn);
+//   }
+// }) 
     
 
-  function addEmployee(title, author, isbn) {
-    const employee = {
-        id: nextId++,
-        title: title,
-        author: author,
-        isbn: parseInt(isbn)
-    };
-    employees.push(employee);
-    displayEmployees();
-}
+//   function addEmployee(title, author, isbn) {
+//     const employee = {
+//         id: nextId++,
+//         title: title,
+//         author: author,
+//         isbn: parseInt(isbn)
+//     };
+//     employees.push(employee);
+//     displayEmployees();
+// }
 
-function deleteEmployee(id) {
-    employees = employees.filter(employee => employee.id !== id);
-    displayEmployees();
-}
+// function deleteEmployee(id) {
+//     employees = employees.filter(employee => employee.id !== id);
+//     displayEmployees();
+// }
 
-function displayEmployees() {
-  let table = document.getElementById("book-list");
-  table.innerHTML = '';
+// function displayEmployees() {
+//   let table = document.getElementById("book-list");
+//   table.innerHTML = '';
 
-    employees.forEach(employee => {
+//     employees.forEach(employee => {
       
-      let tr = document.createElement('tr');
-        tr.innerHTML = `
-        <td>${employee.title}</td>
-        <td>${employee.author}</td>
-        <td>${employee.isbn}</td>    
-        <button onclick="deleteEmployee(${employee.id})">Clear</button>
-        `;
-        table.append(tr);
-    });
-}
+//       let tr = document.createElement('tr');
+//         tr.innerHTML = `
+//         <td>${employee.title}</td>
+//         <td>${employee.author}</td>
+//         <td>${employee.isbn}</td>    
+//         <button onclick="deleteEmployee(${employee.id})">Clear</button>
+//         `;
+//         table.append(tr);
+//     });
+// }
